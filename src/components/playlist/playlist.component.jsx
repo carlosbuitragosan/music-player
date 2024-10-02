d8bfdcc1b3299ae82490055bb71a3afb9c5f817a
Carlos Buitrago <letscatchupagain@gmail.com>
2024-10-02 16:36:47 +0100

    Add input to add or edit the playlist title.

--- src/components/playlist/playlist.component.jsx
+++ src/components/playlist/playlist.component.jsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Playlist({ playlist, removeTrack }) {
+  const [title, setTitle] = useState('');
+
+  const addTitle = ({ target }) => {
+    setTitle(target.value);
+  };
   const handleClick = (trackId) => {
     return () => removeTrack(trackId);
   };
 
   return (
     <div>
-      <h2>Your Playlist</h2>
+      <input
+        type="text"
+        placeholder="Add a title"
+        value={title}
+        onChange={addTitle}
+      />
       {playlist.map((track) => (
         <div key={track.id}>
           <img alt={track.name} src={track.album.images[0].url} width="50" />
